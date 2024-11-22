a5a7634c3c1ab7ca2b0bb049158652dbcaf0f1a8
matheus.mmarcondes <matheus.mmarcondes@SJC0562930W10-1>
2024-11-22 10:16:44 -0300

    Adicionar arquivos de projeto.

---
+++ ExFibonacci/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ExFibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsbSequencia = new System.Windows.Forms.ListBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsbSequencia
+            // 
+            this.lsbSequencia.FormattingEnabled = true;
+            this.lsbSequencia.Location = new System.Drawing.Point(293, 34);
+            this.lsbSequencia.Name = "lsbSequencia";
+            this.lsbSequencia.Size = new System.Drawing.Size(169, 264);
+            this.lsbSequencia.TabIndex = 0;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(338, 346);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 50);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(693, 373);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lsbSequencia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lsbSequencia;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ ExFibonacci/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExFibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lsbSequencia.Items.Clear();
+            int a = 0, b = 1;
+            lsbSequencia.Items.Add(a);
+            lsbSequencia.Items.Add(b);
+
+            for (int i = 2; i < 30; i++)
+            {
+                int proximo = a + b;
+                lsbSequencia.Items.Add(proximo);
+                a = b;
+                b = proximo;
+            }
+        }
+    }
+
+}
